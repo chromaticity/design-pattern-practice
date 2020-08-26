c2a2ff2fb1f2c3a0cb181457096f4d5a8c2d1938
Brandon Antillon <bantillon@localhost.localdomain>
2020-08-26 08:08:31 -0700

    Added more changes

--- observer_pattern/observer.py
+++ observer_pattern/observer.py
@@ -1,7 +1,10 @@
 # Observer interface for all of our observers.
+from typing import List
+
+
 class Observer:
 
-    def update(self) -> None:
+    def update(self, subject: Subject) -> None:
         pass
 
 
@@ -18,4 +21,44 @@ class Subject:
         pass
 
 
-# Now, make your subjects/observers and have them fuck with each other....
+# message board; notifies students of new assignments and stuff
+class MessageBoard(Subject):
+    # Message that will be given to all students
+    message: str = None
+
+    # List of all students; subscribed to the MessageBoard
+    _allObservers: List[Observer] = []
+
+    def attach(self, student: Observer) -> None:
+        self._allObservers.append(student)
+
+    def detach(self, student: Observer) -> None:
+        self._allObservers.remove(student)
+
+    def notify(self) -> None:
+        for student in self._allObservers:
+            student.update(self)
+
+    def send_message(self, message) -> None:
+        print("Sending message...\n")
+        self.message = message
+        self.notify()
+        print("Message sent, users notified.\n")
+
+
+class Student(Observer):
+    def update(self, subject: MessageBoard) -> None:
+        if subject.message == "ping":
+            print("Student reacted to ping event... \n")
+
+
+if __name__ == "__main__":
+
+    message_board = MessageBoard()
+    student = Student()
+
+    message_board.attach(student)
+
+    message_board.send_message('ping')
+
+    message_board.detach(student)
